f46305d699e62b41b399ab42c35dd1b2e0fc575d
Gerson <gersonlange@gmail.com>
2018-12-09 13:33:07 -0200

    first commit

---
+++ src/main/java/br/edu/unidavi/trabalho/SpringTrabalhoApplication.java
@@ -0,0 +1,12 @@
+package br.edu.unidavi.trabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrabalhoApplication.class, args);
+	}
+}
